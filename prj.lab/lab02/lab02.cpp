68a4564182e241fbcc0e54c4d7a9b74969316f91
sssemion <ssemi.on@yandex.ru>
2024-06-09 22:31:34 +0300

    lab02 final

--- prj.lab/lab02/lab02.cpp
+++ prj.lab/lab02/lab02.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/opencv.hpp>
+#include <random>
 #include <vector>
 
 cv::Mat generateTestImage(int brightness1, int brightness2, int brightness3) {
@@ -23,71 +24,75 @@ cv::Mat generateTestImage(int brightness1, int brightness2, int brightness3) {
 }
 
 cv::Mat drawHistogram(const cv::Mat& img) {
-    int histSize = 256;    // bin size
-    float range[] = { 0, 256 };
-    const float* histRange = { range };
-    bool uniform = true, accumulate = false;
-    cv::Mat hist;
+    cv::Mat histogramImage(256, 256, CV_8UC1, cv::Scalar(230));
 
-    calcHist(&img, 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
-    normalize(hist, hist, 0, 230, cv::NORM_MINMAX);
-
-    int hist_w = 256, hist_h = 256;
-    cv::Mat histImage(hist_h, hist_w, CV_8UC1, cv::Scalar(230));
-
-    for (int i = 0; i < histSize; i++) {
-        line(histImage, cv::Point(i, hist_h), cv::Point(i, hist_h - cvRound(hist.at<float>(i))), cv::Scalar(0), 1, 8, 0);
+    std::vector<int> histogram(256, 0);
+    for (int y = 0; y < img.rows; ++y) {
+        for (int x = 0; x < img.cols; ++x) {
+            int intensity = (int)(img.at<uchar>(y, x));
+            histogram[intensity]++;
+        }
     }
 
-    return histImage;
+    // Нормируем гистограмму, чтобы максимальное значение имело высоту 230
+    int maxHistogramValue = *std::max_element(histogram.begin(), histogram.end());
+    for (int& value : histogram) {
+        value = (int)((double)(value) / maxHistogramValue * 230);
+    }
+
+    for (int i = 0; i < 256; ++i) {
+        int binValue = histogram[i];
+        cv::line(histogramImage, cv::Point(i, 256),
+                 cv::Point(i, 256 - binValue), cv::Scalar(0), 1);
+    }
+
+    return histogramImage;
 }
 
-cv::Mat addNoise(const cv::Mat& img, double sigma) {
-    cv::Mat noise = cv::Mat::zeros(img.size(), img.type());
-    randn(noise, 0, sigma);
-    cv::Mat noised_image;
-    img.convertTo(noised_image, CV_32F);
-    noised_image += noise;
-    noised_image.convertTo(noised_image, CV_8UC1);
-    return noised_image;
+void addNoise(cv::Mat& img, double sigma) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0, sigma);
+
+    for (int i = 0; i < img.rows; ++i) {
+        for (int j = 0; j < img.cols; ++j) {
+            int new_value = img.at<cv::Vec3b>(i, j)[0] + (int)(distribution(generator));
+            new_value = std::min(255, std::max(0, new_value));
+            img.at<cv::Vec3b>(i, j)[0] = new_value;
+        }
+    }
 }
+
 
 void generateAndDisplay() {
     std::vector<std::vector<int>> brightness_levels = { {0, 127, 255}, {20, 127, 235}, {55, 127, 200}, {90, 127, 165} };
-    std::vector<double> sigmas = { 3.0, 7.0, 15.0 };
+    std::vector<double> sigmas = { 0.0, 3.0, 7.0, 15.0 };
 
     cv::Mat combined_image;
-
-    // Generate and combine test images
-    for (auto levels : brightness_levels) {
-        cv::Mat test_img = generateTestImage(levels[0], levels[1], levels[2]);
-        if (combined_image.empty()) {
-            combined_image = test_img;
-        } else {
-            hconcat(combined_image, test_img, combined_image);
-        }
-    }
 
     for (double sigma : sigmas) {
         cv::Mat combined_noised_image;
         for (auto levels : brightness_levels) {
             cv::Mat test_img = generateTestImage(levels[0], levels[1], levels[2]);
-            cv::Mat noised_img = addNoise(test_img, sigma);
-            cv::Mat hist_img = drawHistogram(noised_img);
+            addNoise(test_img, sigma);
+            cv::Mat hist_img = drawHistogram(test_img);
 
             if (combined_noised_image.empty()) {
-                vconcat(noised_img, hist_img, combined_noised_image);
+                vconcat(test_img, hist_img, combined_noised_image);
             } else {
                 cv::Mat temp;
-                vconcat(noised_img, hist_img, temp);
+                vconcat(test_img, hist_img, temp);
                 hconcat(combined_noised_image, temp, combined_noised_image);
             }
         }
 
-        vconcat(combined_image, combined_noised_image, combined_image);
+        if (combined_image.empty()) {
+            combined_image = combined_noised_image;
+        } else {
+            vconcat(combined_image, combined_noised_image, combined_image);
+        }
     }
 
-    imshow("Combined Image", combined_image);
+    imshow("Lab02", combined_image);
     cv::waitKey(0);
 }
 
